47716f35c0515800610ddd0a46d86b4979e1a57c
Pavlo Popeskul <ppopeskul@lohika.com>
2021-07-14 17:18:16 +0300

    lesson_5_controller_tests: added specs for questions_controller#destroy

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :find_question, only: %i[show edit update]
+  before_action :find_question, only: %i[show edit update destroy]
 
   def index
     @questions = Question.all
@@ -31,6 +31,11 @@ class QuestionsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to questions_path
   end
 
   private
