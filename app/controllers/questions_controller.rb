d04f3859258c5d72a807f23badbfd968ea4adff1
Pavlo Popeskul <ppopeskul@lohika.com>
2021-07-14 16:17:07 +0300

    lesson_5_controller_tests: added specs for questions_controller#create

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,10 +12,24 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
   def edit
   end
 
   private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 
   def find_question
     @question = Question.find(params[:id])
