a7c7d5c39771bd6bf94e22d27f390e6c7afd69b7
Pavlo Popeskul <ppopeskul@lohika.com>
2021-07-14 17:36:13 +0300

    lesson_5_controller_tests: removed before_action for find_question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,11 @@
 class QuestionsController < ApplicationController
-  before_action :find_question, only: %i[show edit update destroy]
-
   def index
     @questions = Question.all
   end
 
-  def show
-  end
+  def show; end
 
-  def new
-    @question = Question.new
-  end
+  def new; end
 
   def create
     @question = Question.new(question_params)
@@ -26,15 +21,15 @@ class QuestionsController < ApplicationController
   end
 
   def update
-    if @question.update(question_params)
-      redirect_to @question
+    if question.update(question_params)
+      redirect_to question
     else
       render :edit
     end
   end
 
   def destroy
-    @question.destroy
+    question.destroy
     redirect_to questions_path
   end
 
@@ -44,7 +39,9 @@ class QuestionsController < ApplicationController
     params.require(:question).permit(:title, :body)
   end
 
-  def find_question
-    @question = Question.find(params[:id])
+  helper_method :question
+
+  def question
+    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
   end
 end
