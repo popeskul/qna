d39ecd75c0c0fe364f280deb58035db6cdd59127
Pavlo Popeskul <ppopeskul@lohika.com>
2021-07-14 17:01:45 +0300

    lesson_5_controller_tests: added specs for questions_controller#update

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :find_question, only: %i[show edit]
+  before_action :find_question, only: %i[show edit update]
 
   def index
     @questions = Question.all
@@ -23,6 +23,14 @@ class QuestionsController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
   end
 
   private
