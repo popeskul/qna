d6eaf3d6ac302e511b411019b4aba525a62b2756
Pavlo Popeskul <ppopeskul@lohika.com>
2021-07-16 11:57:37 +0300

    lesson_5_controller_tests: added specs for answer_controller#new

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
+require 'pp'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
+  let(:answer) { create(:answer) }
 
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question } }
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
 end
