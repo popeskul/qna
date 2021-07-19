d4e8c25d5f1035d388a9d3bfc39592f3ff36170e
Pavlo Popeskul <ppopeskul@lohika.com>
2021-07-19 20:14:41 +0300

    lesson_6_acceptance_testing: User can view a question and its answers spec

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -6,5 +6,14 @@ FactoryBot.define do
     trait :invalid do
       title { nil }
     end
+
+    factory :question_with_answers, class: Question do
+      title { Faker::Lorem.unique.sentence }
+      body { Faker::Lorem.unique.paragraph }
+
+      after(:create) do |question_with_answers, _evaluator|
+        create_list(:answer, 2, question: question_with_answers)
+      end
+    end
   end
 end
