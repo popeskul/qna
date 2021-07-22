f954b36b28b42ef58f0f642283d3609a6bbb2d1e
popeskul <pavel.popeskul@gmail.com>
2021-07-22 15:40:42 +0300

    lesson_6_acceptance_testing: spec Author_of? user mdoel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_20_204946) do
+ActiveRecord::Schema.define(version: 2021_07_22_123507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,9 @@ ActiveRecord::Schema.define(version: 2021_07_20_204946) do
     t.bigint "question_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -46,5 +48,6 @@ ActiveRecord::Schema.define(version: 2021_07_20_204946) do
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
   add_foreign_key "questions", "users"
 end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :answer do
     body { Faker::Lorem.unique.sentence }
+    association :author, factory: :user
 
     trait :invalid do
       body { nil }
