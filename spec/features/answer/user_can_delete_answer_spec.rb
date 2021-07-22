189c30ce2f98171300914aedae32b391ee9de52a
popeskul <pavel.popeskul@gmail.com>
2021-07-23 01:16:03 +0300

    lesson_6_acceptance_testing: fixed User can delete his answer

--- spec/features/answer/user_can_delete_answer_spec.rb
+++ spec/features/answer/user_can_delete_answer_spec.rb
@@ -1,36 +1,39 @@
 require 'rails_helper'
 
 feature 'User can delete his answer' do
-  let(:user) { create(:user)}
+  let(:user) { create(:user) }
   let(:user1) { create(:user) }
+
   let(:question) { create(:question, author: user) }
   let(:question1) { create(:question, author: user1) }
 
-  scenario 'Authenticated user can delete his answer' do
-    sign_in(user)
+  context 'Authenticated user' do
+    before { sign_in(user) }
 
-    answer = create(:answer, question: question, author: user)
-    visit question_path(question)
+    scenario 'Authenticated user can delete his answer' do
+      answer = create(:answer, question: question, author: user)
+      visit question_path(question)
 
-    click_link href: answer_path(answer)
+      click_link href: answer_path(answer)
 
-    expect(current_path).to eq question_path(question)
-    expect(page).to have_content 'Answer was successfully deleted'
+      expect(current_path).to eq question_path(question)
+      expect(page).to have_content 'Answer was successfully deleted'
+    end
+
+    scenario "Authenticated user can not delete another''s answer" do
+      answer = create(:answer, question: question)
+
+      visit question_path(question1)
+
+      expect(page).to have_no_link('Delete', href: answer_path(answer))
+    end
   end
 
-  scenario "Authenticated user can not delete another''s answer" do
-    sign_in(user1)
+  context 'Non-authenticated user' do
+    scenario "Non-authenticated user can not delete another''s answer" do
+      answer = create(:answer, question: question1)
 
-    answer = create(:answer, question: question)
-
-    visit question_path(question1)
-
-    expect(page).to have_no_link('Delete', href: answer_path(answer))
-  end
-
-  scenario "Non-authenticated user can not delete another''s answer" do
-    answer = create(:answer, question: question1)
-
-    expect(page).to have_no_link('Delete', href: answer_path(answer))
+      expect(page).to have_no_link('Delete', href: answer_path(answer))
+    end
   end
 end
