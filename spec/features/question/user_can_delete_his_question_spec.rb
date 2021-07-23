a7280be4ba914e3b00a07e5011184d8c4d9b0d93
popeskul <pavel.popeskul@gmail.com>
2021-07-23 13:53:28 +0300

    lesson_6_acceptance_testing: Non-authenticated user can't delete question spec

--- spec/features/question/user_can_delete_his_question_spec.rb
+++ spec/features/question/user_can_delete_his_question_spec.rb
@@ -4,9 +4,10 @@ feature 'User can delete his question' do
   let(:user) { create(:user) }
   let(:user1) { create(:user) }
 
+  let(:question) { create(:question, author: user) }
+
   describe 'Authenticated user' do
     background { sign_in(user) }
-    let(:question) { create(:question, author: user) }
 
     scenario 'Authenticated user can delete his question' do
       visit question_path(question)
@@ -27,6 +28,14 @@ feature 'User can delete his question' do
       visit question_path(question)
 
       expect(page).to have_no_content 'Delete question'
+    end
+  end
+
+  describe 'Non-authenticated user' do
+    scenario "Non-authenticated user can't delete question" do
+      visit question_path(question)
+
+      expect(page).to have_no_content('Delete question')
     end
   end
 end
