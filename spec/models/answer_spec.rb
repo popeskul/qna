904adda239c2a3a90e55d7c2a0fafbe8618db77a
popeskul <pavel.popeskul@gmail.com>
2021-07-25 23:54:34 +0300

    lesson_7_ajax: refactored answer spec

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it { should belong_to(:question) }
+  describe 'Associations' do
+    it { should belong_to(:question) }
+    it { should belong_to(:author) }
+  end
 
-  it { should validate_presence_of :body }
+  describe 'Validations' do
+    it { should validate_presence_of :body }
+  end
 end
