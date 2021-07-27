e4282c10f0d3cd63c3ee943333a07aca094a883a
popeskul <pavel.popeskul@gmail.com>
2021-07-27 16:08:59 +0300

    lesson_8_active_storage: multiple files and spec

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,8 @@ class Question < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: 'author_id'
 
   has_many :answers, dependent: :destroy
-  has_one_attached :file
+
+  has_many_attached :files
 
   validates :title, :body, presence: true
 end
