a85d18257cbfe4dd775e399b73fb6e0e0bbeedb8
popeskul <pavel.popeskul@gmail.com>
2021-08-02 14:33:47 +0300

    lesson_9_nested_form_polymorphic: The author of the question can add and edit a link

--- spec/features/question/user_can_add_links_spec.rb
+++ spec/features/question/user_can_add_links_spec.rb
@@ -7,6 +7,8 @@ feature 'User can add links to question', "
 " do
   given(:user) { create(:user) }
 
+  given!(:question) { create(:question, author: user) }
+
   given(:url) { 'https://www.google.com/' }
   given(:url2) { 'https://www.youtube.com/' }
 
@@ -16,14 +18,14 @@ feature 'User can add links to question', "
     background do
       sign_in(user)
       visit new_question_path
-
-      fill_in 'Title', with: 'Test question'
-      fill_in 'Body', with: 'text text text'
     end
 
     scenario 'User adds many links to question', js: true do
       link_name = 'Link name'
       link_name2 = 'Link name 2'
+
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'text text text'
 
       fill_in 'Link name', with: link_name
       fill_in 'Url', with: url
@@ -48,6 +50,63 @@ feature 'User can add links to question', "
       click_on 'Ask'
 
       expect(page).to have_content 'Links url must be a valid URL'
+    end
+
+    scenario 'The author of the question can add a new links', js: true do
+      link_name = 'Link name'
+
+      visit questions_path
+
+      within '.questions' do
+        click_on 'Edit'
+
+        click_on 'Add link'
+
+        within '.question' do
+          fill_in 'Link name', with: link_name
+          fill_in 'Url', with: url2
+        end
+
+        click_on 'Save'
+      end
+
+      visit question_path(question)
+
+      expect(page).to have_link link_name, href: url2
+    end
+
+    scenario 'The author of the question can edit a link', js: true do
+      link_name = 'Link name'
+      link_name2 = 'Link name 2'
+
+      visit questions_path
+
+      click_on 'Edit'
+      click_on 'Add link'
+
+      within '.questions' do
+        fill_in 'Link name', with: link_name
+        fill_in 'Url', with: url
+
+        click_on 'Save'
+      end
+
+      visit questions_path
+
+      within '.questions' do
+        click_on 'Edit'
+
+        within '.question' do
+          fill_in 'Link name', with: link_name2
+          fill_in 'Url', with: url2
+        end
+
+        click_on 'Save'
+      end
+
+      visit question_path(question)
+
+      expect(page).to have_link link_name2, href: url2
     end
   end
 end
