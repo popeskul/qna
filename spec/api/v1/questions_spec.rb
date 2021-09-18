4deee2abb22b465c4ede5097d94fb89516fcb52c
Pavel Popeskul <peterjj91@gmail.com>
2021-09-18 20:39:55 +0300

    lesson_14_rest_api: fixed question describe name

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -4,7 +4,7 @@ describe 'Questions API', type: :request do
   let(:headers) { { 'ACCEPT' => 'application/json' } }
   let(:api_path) { '/api/v1/questions' }
 
-  describe 'GET /api/v1/profiles/me' do
+  describe 'GET /api/v1/questions' do
     it_behaves_like 'API Authorizable' do
       let(:method) { :get }
     end
