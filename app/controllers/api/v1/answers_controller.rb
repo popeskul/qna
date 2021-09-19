ac3d3066b435b798927af0338cea38fb4e7e2ad6
Pavel Popeskul <peterjj91@gmail.com>
2021-09-19 15:17:29 +0300

    lesson_14_rest_api: POST /api/v1/questions/:id/answers, PATCH /api/v1/answers/:id, DELETE /api/v1/answers/:id

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -15,6 +15,36 @@ module Api
       def show
         render json: answer, serializer: AnswerDataSerializer
       end
+
+      def create
+        @answer = question.answers.new(answer_params)
+        @answer.author = current_resource_owner
+
+        flash.now[:notice] = 'The answer was created successfully.' if @answer.save
+      end
+
+      def destroy
+        answer.destroy
+        flash.now[:notice] = 'Answer was successfully deleted'
+      end
+
+      def update
+        answer.update(answer_params)
+        flash.now[:notice] = 'The answer was updated successfully.'
+
+        @answer = answer
+      end
+
+      def set_as_the_best
+        answer.set_the_best_answer
+        @question = answer.question
+      end
+
+      private
+
+      def answer_params
+        params.require(:answer).permit(:body, files: [], links_attributes: %i[name url])
+      end
     end
   end
 end
--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -85,4 +85,132 @@ describe 'Answers API', type: :request do
       end
     end
   end
+
+  describe 'POST /api/v1/questions/:id/answers' do
+    let(:user)         { create(:user) }
+    let(:question)     { create(:question, author: user) }
+
+    let(:api_path)     { "/api/v1/questions/#{question.id}/answers" }
+    let(:access_token) { create(:access_token) }
+
+    context 'with valid attributes' do
+      let(:params) { { question_id: question.id, answer: attributes_for(:answer), access_token: access_token.token } }
+      subject { post api_path, params: params }
+
+      it 'save a new answer' do
+        expect { subject }.to change { question.answers.reload.count }.by(1)
+      end
+
+      it 'redirects to show' do
+        subject
+        expect(response.status).to eq 204
+      end
+    end
+
+    context 'with invalid attributes' do
+      let(:params) { { question_id: question.id, answer: attributes_for(:answer, :invalid) } }
+      let(:create_answer) { post api_path, params: params }
+
+      it 'does not save the new answer in the database' do
+        expect { create_answer }.to_not change(question.answers, :count)
+      end
+
+      it 're-renders new view' do
+        create_answer
+        expect(response.status).to eq 401
+      end
+    end
+  end
+
+  describe 'PATCH /api/v1/answers/:id' do
+    let(:user)         { create(:user) }
+    let(:another_user) { create(:user) }
+
+    let(:question)     { create(:question, author: user) }
+    let(:answer)       { create(:answer, question: question) }
+
+    let(:access_token) { create(:access_token) }
+    let(:api_path)     { "/api/v1/answers/#{answer.id}" }
+
+    let(:update_answer) do
+      patch api_path, params: {
+        id: answer,
+        question_id: question.id,
+        answer: attributes_for(:answer),
+        access_token: access_token.token
+      }
+    end
+
+    context 'if answer belongs to the user' do
+      it 'assigns the requested answer to @answer' do
+        update_answer
+        expect(assigns(:answer)).to eq answer
+      end
+
+      it 'assigns the requested answers question to @question' do
+        update_answer
+        expect(assigns(:answer).question).to eq question
+      end
+
+      it 'changes answer attributes' do
+        patch api_path, params: {
+          id: answer,
+          question_id: question,
+          answer: { body: 'new body' },
+          access_token: access_token.token
+        }
+
+        answer.reload
+        expect(answer.body).to eq 'new body'
+      end
+
+      it 'renders update template' do
+        update_answer
+        answer.reload
+        expect(response.status).to eq 204
+      end
+    end
+  end
+
+  describe 'DELETE /api/v1/answers/:id' do
+    let!(:user) { create(:user) }
+    let(:question) { create(:question, author: user) }
+    let!(:answer) { create(:answer, question: question) }
+
+    let(:access_token) { create(:access_token) }
+    let(:api_path)     { "/api/v1/answers/#{answer.id}" }
+
+    let(:delete_answer) do
+      delete api_path, headers: headers, params: { id: answer, question_id: question, access_token: access_token.token }
+    end
+
+    context 'if answer belongs to the user', js: true do
+      it 'deletes answer' do
+        expect { delete_answer }.to change(Answer, :count).by(-1)
+      end
+
+      it 'render view destroy' do
+        delete_answer
+        expect(response.status).to eq 204
+      end
+    end
+
+    context 'if answer does not belong to the user' do
+      let(:other_user)     { create(:user) }
+      let(:other_answer)   { create(:answer, question: question, author: other_user) }
+      let(:other_api_path) { "/api/v1/answers/#{other_answer.id}" }
+      let(:params)         { { access_token: access_token.token, id: other_answer } }
+
+      let(:does_not_delete_answer) { delete other_api_path, headers: headers, params: params }
+
+      it 'does not delete answer' do
+        expect { does_not_delete_answer }.to_not change(Answer, :count)
+      end
+
+      it 'redirects to question#show' do
+        does_not_delete_answer
+        expect(response).to_not redirect_to question
+      end
+    end
+  end
 end
