82b093374b8e56e90718bf73fc6e3cec2d48e68f
Pavlo Popeskul <ppopeskul@lohika.com>
2021-10-02 01:50:53 +0300

    lesson_15_background_tasks: done with tasks

--- app/services/services/daily_digest.rb
+++ app/services/services/daily_digest.rb
@@ -3,7 +3,7 @@
 # DailyDigestMailer service
 module Services
   # implement DailyDigestMailer
-  class DailyDigestMailer
+  class DailyDigest
     def send_digest
       User.find_each(batch_size: 500) do |user|
         DailyDigestMailer.digest(user).deliver_later
